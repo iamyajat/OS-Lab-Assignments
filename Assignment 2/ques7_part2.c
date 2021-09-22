94a9a860a55819787d5a170ec9dc4aceb8c6f201
iamyajat <myajat21@gmail.com>
2021-09-22 20:07:02 +0530

    fix: bg process prog fixed

--- Assignment 2/ques7_part2.c
+++ Assignment 2/ques7_part2.c
@@ -4,21 +4,21 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int main() {
-    printf(
-        "Process in running in background. Press q to finish the background "
-        "process\n");
-    while (1) {
-        printf("Enter 'q' to kill process...\n");
-        char c = getchar();
-        // sleep(10);
-        if (c == 'q') {
-            printf("You killed the background process\n");
-            exit(1);
-        }
+int main(void)
+{
+   pid_t pid = fork();
 
-        sleep(1);
-    }
-    printf("Process is out of sleep after 10 seconds\n");
-    return 0;
+   if (pid == -1) {
+      perror("fork failed");
+      exit(1);
+   }
+   else if (pid == 0) {
+      printf("Hello from the child process!\n");
+      exit(0);
+   }
+   else {
+      int status;
+      (void)waitpid(pid, &status, 0);
+   }
+   return 0;
 }
