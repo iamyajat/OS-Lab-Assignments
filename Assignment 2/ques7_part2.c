03ec6ee28684e375f77c3f4795d00d39e69820b1
iamyajat <myajat21@gmail.com>
2021-09-22 20:18:21 +0530

    fix: bg process prog fixed

--- Assignment 2/ques7_part2.c
+++ Assignment 2/ques7_part2.c
@@ -1,24 +1,27 @@
-// background process
-#include <ctype.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include <sys/wait.h>
+#include <sys/types.h>
 #include <unistd.h>
+#include <stdio.h>
 
-int main(void)
+int main()
 {
-   pid_t pid = fork();
-
-   if (pid == -1) {
-      perror("fork failed");
-      exit(1);
-   }
-   else if (pid == 0) {
-      printf("Hello from the child process!\n");
-      exit(0);
-   }
-   else {
-      int status;
-      (void)waitpid(pid, &status, 0);
-   }
-   return 0;
+    int pid = fork();
+    if (pid)
+    {
+        int p = 0;
+        int status;
+        sleep(60);
+        p = wait(&status);
+        printf("Pid %d exited...\n", p);
+    }
+    else
+    {
+        for(int i = 0; i < 10; i++)
+        {
+            printf("Child is sleeping... %d\n";
+            sleep(1);
+        }
+        printf("Child exited...");
+    }
+    return 0;
 }
